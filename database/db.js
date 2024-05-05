97d22f5f719b5c00c022c1ebbc3b550be23b309d
Habibi <134970522+X0Rhyth@users.noreply.github.com>
2024-05-05 19:35:12 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const dbPassword = process.env.DBPASSWORD;
 const pool = new Pool({
   user: dbUser,
   host: 'dpg-cos0pqvsc6pc73dv3etg-a.oregon-postgres.render.com',
-  database: 'yelpcamp',
+  database: 'yelpcamp_rq7f',
   password: dbPassword,
   port: 5432, // alt PostgreSQL port
   ssl: true
