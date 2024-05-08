4ac4929ab8ab5c30b2543035c9f42d2b7e31119f
MUSKAN568U9 <162317019+MUSKAN568U9@users.noreply.github.com>
2024-05-08 23:50:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -21,14 +21,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelpcamp'
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
