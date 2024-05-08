ee2683567bb674e3e7696e52cbfab50c61189312
MUSKAN568U9 <162317019+MUSKAN568U9@users.noreply.github.com>
2024-05-08 23:57:26 +0500

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,11 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
 
 const db = mongoose.connection;
 
